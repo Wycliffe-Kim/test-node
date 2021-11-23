7731fa154791345ab26cbfa9fdead755db93f712
Wycliffe-Kim <f16o@hanyang.ac.kr>
2021-11-23 17:01:42 +0900

    c

--- index.ts
+++ index.ts
@@ -1,31 +1,11 @@
 import _ from 'lodash';
-// import R from 'ramda';
-const R = require('ramda');
 
-class Person {
-  name: string;
-  country: string;
-
-  constructor(name, country) {
-    this.name = name;
-    this.country = country;
+function sum(arr: any[], acc = 0) {
+  if (_(arr).isEmpty()) {
+    return 0;
   }
+
+  return acc + _(arr).first() + sum(_(arr).slice(1).value());
 }
 
-const countryLens = R.lens(R.prop('country'), R.assoc('country'));
-
-const persons: Person[] = [
-  new Person('person1', 'US'),
-  new Person('person2', 'US'),
-  new Person('person3', 'Greece'),
-  new Person('person4', 'Hungary'),
-];
-
-const getCountry = person => person.country;
-const gatherStats = (stat, country) => {
-  stat[country] = _.isUndefined(stat[country]) ? 1 : stat[country] + 1;
-  return stat;
-}
-
-const data = _(persons).map(R.view(countryLens)).reduce(gatherStats, {});
-console.log(data);
+console.log(sum([1, 2, 3, 4], 10));
