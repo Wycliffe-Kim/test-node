2ba9cdf3072add918a419f5cbe1c3f3e2a79cba7
김원동 <wycliffe@gim-wondong-ui-MacBookAir.local>
2021-12-28 14:39:22 +0900

    c;

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-function doWork() {
-  if (!myVar) {
-    var myVar = 10;
+const R = require('ramda');
+
+class Wrapper {
+  constructor(value) {
+    this._value = value;
   }
-  console.log(myVar);
+
+  map(f) {
+    return f(this._value);
+  }
+
+  fmap(f) {
+    return new Wrapper(f(this._value));
+  }
 }
+
+const two = new Wrapper(2);
+const add = R.curry((a, b) => a + b);
+
+console.log(two.fmap(add(3)).fmap(add(2)).map(R.identity)); // 7
