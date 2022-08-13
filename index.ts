3aa297defa67a81438dbed9185ceb6cd2e4d46be
Wycliffe-Kim <f16o@hanyang.ac.kr>
2022-08-13 15:01:09 +0900

    test merge 18

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import { test1 } from './test1';
 
 const endTime = new Date();
 
@@ -10,4 +11,11 @@ const startTime = new Date(endTime.getTime() - hourToMs(1));
 
 console.log(endTime, startTime);
 
-console.log('test merge 17');
+console.log('test merg 18');
+
+console.log('added1');
+console.log('added2');
+console.log('added3');
+console.log('added4');
+
+test1();
