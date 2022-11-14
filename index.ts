6967e8eb0f1ba8e5679672680a06aca65400f49a
Wycliffe-Kim <80871550+Wycliffe-Kim@users.noreply.github.com>
2022-11-14 16:46:28 +0900

    test merge 21 (#21)

--- index.ts
+++ index.ts
@@ -1,16 +1,22 @@
-import _ from 'lodash';
-import { test1 } from './test1';
+import moment from 'moment-timezone';
 
-const endTime = new Date();
+const splitDateTimeString = (dateTime: string) => {
+  const utc = moment.utc(dateTime).toDate();
+  const _dateTime = moment(utc).local().format('YYYY-MM-DDTHH:mm:ss');
+  const date = _dateTime.split('T')[0];
+  const time = _dateTime.split('T')[1].split('.')[0];
+  const hour = `${+time.split(':')[0]}`.padStart(2, '0');
+  const min = `${time.split(':')[1]}`.padStart(2, '0');
+  const sec = `${time.split(':')[2]}`.padStart(2, '0');
 
-const hourToMs = (hour: number) => hour * 60 * 60 * 1000;
+  return { date, hour, min, sec };
+};
 
-const msToHour = (ms: number) => ms / (60 * 60 * 1000);
+const date = '2022-09-19T04:59:00';
+const utc = moment.utc(date).toDate();
+console.log(utc);
 
-const startTime = new Date(endTime.getTime() - hourToMs(1));
+const d = moment(utc).local().format('YYYY-MM-DDThh:mm:ss');
+console.log(d);
 
-console.log(endTime, startTime);
-
-console.log('test merge 20');
-
-test1();
+console.log(splitDateTimeString(date));
