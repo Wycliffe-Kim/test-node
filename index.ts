01efd65de60f68931412601c606dcbc99dfb35b8
Wycliffe-Kim <f16o@hanyang.ac.kr>
2023-06-07 15:55:52 +0900

    index 파일 변경

--- index.ts
+++ index.ts
@@ -8,37 +8,7 @@
  * Email: contact@nota.ai
  */
 
-type DateToStringOptions = Partial<{
-  isIso: boolean;
-  containMilliseconds: boolean;
-}>;
-const dateToString = (
-  date: Date,
-  options: DateToStringOptions = {
-    isIso: false,
-    containMilliseconds: false,
-  },
-) => {
-  const { isIso, containMilliseconds } = options;
-  const year = `${date.getFullYear()}`.padStart(4, '0');
-  const month = `${date.getMonth() + 1}`.padStart(2, '0');
-  const day = `${date.getDate()}`.padStart(2, '0');
-  const hour = `${date.getHours()}`.padStart(2, '0');
-  const min = `${date.getMinutes()}`.padStart(2, '0');
-  const seconds = `${date.getSeconds()}`.padStart(2, '0');
-  const milliseconds = `${date.getMilliseconds()}`.padStart(3, '0');
+import { dateToStringCase, getDateTimeDifferenceCase } from './cases';
 
-  return isIso === true
-    ? containMilliseconds === true
-      ? date.toISOString()
-      : `${date.toISOString().substring(0, 19)}Z`
-    : containMilliseconds === true
-    ? `${year}-${month}-${day} ${hour}:${min}:${seconds}.${milliseconds}`
-    : `${year}-${month}-${day} ${hour}:${min}:${seconds}`;
-};
-
-const date = new Date();
-console.log(dateToString(date));
-console.log(dateToString(date, { containMilliseconds: true }));
-console.log(dateToString(date, { isIso: true }));
-console.log(dateToString(date, { isIso: true, containMilliseconds: true }));
+dateToStringCase();
+getDateTimeDifferenceCase();
